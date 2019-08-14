4ee54dd2c295b6799cef359af666554e1cf5fb46
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2019-08-14 18:07:18 -0400

    comentando algunas cosas para ordenar

--- tarea1/solucion/funciones.py
+++ tarea1/solucion/funciones.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+#P1:
+
+
 
 #Método con  error de órden O(h)
 def DerivadaO1(f,x,h):
@@ -9,9 +12,7 @@ def DerivadaO1(f,x,h):
 def DerivadaO4(f,x,h):
     return (-f(x+2*h)+8*f(x+h)-8*f(x-h)+f(x-2*h))/(12*h)
 
-
-
-
+#funcion que se encarga de preparar toodo para graficar
 def Setup(f,x,iteraciones,datatype,reff=None,g=None):
 
     h=datatype(0.1)**np.arange(iteraciones)
@@ -44,6 +45,10 @@ def Aprox2nd(f,a,b,h):
     return (f(x+h)-2*f(x)+f(x-h))/(h**2)
 
 
+#=======================================================================================================================
+
+
+#P2:
 
 def Conv(I_0, I_1):
     return np.divide(np.abs(I_1-I_0),np.abs(I_0))
@@ -53,18 +58,16 @@ def Trap_iter(f, a, b, k):
     n = 100
     error = .01
     delta = (b-a)/n
-
-
+    #se crean particiones pertinentes
     partition_0 = np.linspace(a,b,n)
     partition_1 = np.linspace(a,b,2*n)
-
+    #evaluaciones de integrales
     I_0=delta*.5*f(partition_0[0],k)+f(partition_0[-1],k)+2*np.sum(f(partition_0[1:-1],k))
-
     I_1=I_0*.5+np.sum(f(partition_1[1::2],k))*delta*.5
-
+    #redefino cositas
     n=2*n
     delta=(b-a)/n
-
+    #criterio de convergencia
     while Conv(I_0, I_1) >= error:
         n=2*n
         p = np.linspace(a,b,n)
@@ -73,5 +76,5 @@ def Trap_iter(f, a, b, k):
         I_1 = I_n
         delta=delta/2
 
-
+    #devuelve una integral mejor basado en las últimas dos calculadas con método de trapecio
     return (4/3)*I_1-(1/3)*I_0
